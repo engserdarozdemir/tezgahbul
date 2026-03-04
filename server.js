abab75df6653f4c2e7509a2783f83ff335fe7fe8
engserdarozdemir <eng.serdarozdemir@gmail.com>
2026-03-04 22:32:56 +0300

    Initial commit: TezgahBul website

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const ROOT = __dirname;
+
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.json': 'application/json',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif':  'image/gif',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+  '.woff': 'font/woff',
+  '.woff2':'font/woff2',
+};
+
+http.createServer((req, res) => {
+  let url = req.url === '/' ? '/index.html' : req.url;
+  const filePath = path.join(ROOT, url.split('?')[0]);
+  const ext = path.extname(filePath).toLowerCase();
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
